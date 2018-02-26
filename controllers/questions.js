81bd49562ad3e08e638b0bcf324921d6e93d6383
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-26 10:07:25 -0500

    backend can check answers

--- controllers/questions.js
+++ controllers/questions.js
@@ -2,16 +2,29 @@ const express             = require('express');
 const Questions           = require('../models/questions.js');
 const router              = express.Router();
 
+//This route sends an array of all question ids ()
 router.get('/', (req,res)=>{
   Questions.find({}, (err,foundQuestions)=>{
     idArray = [];
     for (let i = 0; i < foundQuestions.length; i++){
       idArray.push(foundQuestions[i]._id);
     }
-    console.log(idArray);
     res.json(idArray);
-    // res.json(foundQuestions);
   })
+});
+
+//This route is used to check an answer.  It takes in an object with a single key-value pair {guess:choice} to check against the 'correct' key of the found question
+router.put('/check/:id', (req,res)=>{
+  // console.log(req.body.guess);
+  let correct = false;
+  Questions.findById(req.params.id, (err,foundQuestion)=>{
+    // console.log(foundQuestion.correct);
+    if (req.body.guess === foundQuestion.correct) {
+      correct = true;
+    }
+    console.log(correct);
+  })
+  res.send('received');
 })
 
 router.post('/', (req,res)=>{
