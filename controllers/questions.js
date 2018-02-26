a02a973a8e343d78087d58c9b4d99396bf0a88ce
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-26 09:31:22 -0500

    can get array of question ids

--- controllers/questions.js
+++ controllers/questions.js
@@ -2,6 +2,18 @@ const express             = require('express');
 const Questions           = require('../models/questions.js');
 const router              = express.Router();
 
+router.get('/', (req,res)=>{
+  Questions.find({}, (err,foundQuestions)=>{
+    idArray = [];
+    for (let i = 0; i < foundQuestions.length; i++){
+      idArray.push(foundQuestions[i]._id);
+    }
+    console.log(idArray);
+    res.json(idArray);
+    // res.json(foundQuestions);
+  })
+})
+
 router.post('/', (req,res)=>{
   Questions.create(req.body, (err, createdQuestion)=>{
     res.json(createdQuestion);
