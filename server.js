f77481c060cfcdc52371834e2abe30eca8a1797c
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-26 09:12:45 -0500

    mongoose connected

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express           = require('express');
+const mongoose          = require('mongoose');
 const app               = express();
 const PORT              = 12345;
 
 const questionsController = require('./controllers/questions.js');
 app.use('/questions', questionsController);
 
+mongoose.connect('mongodb://localhost:27017/office');
+mongoose.connection.once('open', ()=>{
+  console.log('Michael Scott, extension ' + PORT);
+})
+
 app.listen(PORT, ()=>{
   console.log('Dunder Mifflin, this is Pam');
-  console.log('Michael Scott, extension ' + PORT)
 })
