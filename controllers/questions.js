2ef19b951657ba950df61f91bac6f1203f57d152
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-26 21:21:04 -0500

    updated delete route

--- controllers/questions.js
+++ controllers/questions.js
@@ -4,6 +4,7 @@ const router              = express.Router();
 
 //This route sends an array of all question ids ()
 router.get('/', (req,res)=>{
+    console.log('index route');
   Questions.find({}, (err,foundQuestions)=>{
     idArray = [];
     for (let i = 0; i < foundQuestions.length; i++){
@@ -15,10 +16,11 @@ router.get('/', (req,res)=>{
 
 //Route to get a random question from the database
 router.get('/random', (req,res)=>{
+  console.log('random route');
   Questions.find({}, (err,foundQuestions)=>{
     let num = Math.floor(Math.random()*foundQuestions.length);
-    console.log(num, 'number')
     let randomQuestion = {
+      id: foundQuestions[num].id,
       answers: foundQuestions[num].answers,
       question: foundQuestions[num].question
     }
@@ -28,6 +30,7 @@ router.get('/random', (req,res)=>{
 
 //Create questions route
 router.post('/', (req,res)=>{
+    console.log('post route');
   Questions.create(req.body, (err, createdQuestion)=>{
     res.json(createdQuestion);
   })
@@ -35,6 +38,7 @@ router.post('/', (req,res)=>{
 
 //This route is used to check an answer.  It takes in an object with a single key-value pair {guess:choice} to check against the 'correct' key of the found question
 router.put('/check/:id', (req,res)=>{
+    console.log('check answer route');
   Questions.findById(req.params.id, (err,foundQuestion)=>{
     if (req.body.guess === foundQuestion.correct) {
       res.send('correct');
@@ -46,6 +50,7 @@ router.put('/check/:id', (req,res)=>{
 
 //This route will flag a question for review by admin.  It receives "flagText" from the body of the request, increments the flagNumber of the question and pushes the flagText into the flags array of the question
 router.put('/flag/:id', (req,res)=>{
+    console.log('flag route');
   Questions.findById(req.params.id, (err,foundQuestion)=>{
     foundQuestion.flags.push(req.body.flagText);
     foundQuestion.flagNumber += 1;
@@ -55,18 +60,27 @@ router.put('/flag/:id', (req,res)=>{
   })
 })
 
+//Delete questions route
+router.delete('/:id', (req,res)=>{
+    console.log('delete route');
+  Questions.findByIdAndRemove(req.params.id, (err,deletedQuestion)=>{
+    res.json(deletedQuestion);
+  })
+})
+
 //Get a question by its id
 router.get('/:id', (req,res)=>{
+    console.log('get by id route');
   Questions.findById(req.params.id, (err,foundQuestion)=>{
     res.send(foundQuestion);
   })
 })
 
-//Delete questions route
-router.delete('/:id', (req,res)=>{
-  Questions.findByIdAndRemove(req.params.id, (err,deletedQuestion)=>{
-    res.send('deleted');
-  })
-})
+// //Delete questions route
+// router.delete('/:id', (req,res)=>{
+//   Questions.findByIdAndRemove(req.params.id, (err,deletedQuestion)=>{
+//     res.send('deleted');
+//   })
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT              = 12345;
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Accept');
   next()
 })
